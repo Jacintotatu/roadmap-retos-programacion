1efcfc443d8cdc0c31390ec69b8dbfbf2b8ac72e
Jacintotatu <jacintoconesa@hotmail.com>
2024-05-29 00:14:10 +0200

    sigo

---
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/Jacintotatu.py
@@ -0,0 +1,27 @@
+""" * DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
+ *   los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
+ *   (o el número de dígitos que quieras)
+ * - También se debe proponer una operación de finalización del programa."""
+
+print('Hola, bienvenido a tu agenda')
+print("""Elige que quieres hacer:
+*Buscar
+*Insertar
+*Eliminar contacto
+""")
+
+eleccion=input()
+
+contactos={}
+
+if eleccion.capitalize() == 'Insertar':
+    nombre = input('Nombre: ')
+    telefono = int(input('Teléfono: '))
+    contactos[nombre]=telefono
+
+print(contactos)
